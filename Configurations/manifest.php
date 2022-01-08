a58187833eaf0417729fb236ef8093a3c475fefb
Rateb Ghiasy <48240306+ratebghiasy@users.noreply.github.com>
2022-01-08 15:19:31 -0500

    Add files via upload

---
+++ Configurations/manifest.php
@@ -0,0 +1,8 @@
+<?php
+    $websiteTitle = "Muay Thai";
+    $websiteAuthor = "Sarwar";
+    $websiteDetails = "";
+
+    $websiteStyleSheets = array('main' => "main.css",'mobile' => "mobile.css" ,"square" => "square.css");
+    $mediaQueries = array('mobile' => "only screen and (max-width: 770px)");
+?>
---
+++ JavaScript/jquery-3.3.1.js
@@ -0,0 +1,9 @@
+function externalLink() {
+        document.getElementById('demo4').innerHTML = "Paragraph Changed.";
+        document.getElementById('demo4').style.fontSize='35px';
+        document.getElementById('demo4').style.color='red';
+}
+
+function myFunction() {
+    document.getElementById('demo3').innerHTML = "My first JavaScript.";
+}
