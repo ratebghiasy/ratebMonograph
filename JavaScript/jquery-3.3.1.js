42223809a36ebe862c3fbb244cdceb0aa2858832
Rateb Ghiasy <48240306+ratebghiasy@users.noreply.github.com>
2022-01-29 19:54:31 -0500

    Delete jquery-3.3.1.js

--- JavaScript/jquery-3.3.1.js
+++
@@ -1,9 +0,0 @@
-function externalLink() {
-        document.getElementById('demo4').innerHTML = "Paragraph Changed.";
-        document.getElementById('demo4').style.fontSize='35px';
-        document.getElementById('demo4').style.color='red';
-}
-
-function myFunction() {
-    document.getElementById('demo3').innerHTML = "My first JavaScript.";
-}
