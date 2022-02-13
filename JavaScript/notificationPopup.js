1b76427261845fe4da77eec08286fcb8aeea17ae
Rateb Ghiasy <48240306+ratebghiasy@users.noreply.github.com>
2022-02-13 11:26:42 -0500

    Delete notificationPopup.js

--- JavaScript/notificationPopup.js
+++
@@ -1,7 +0,0 @@
-const targetDiv = document.body;
-const div = document.getElementById("notification-btn");
-console.log({targetDiv,div});
-div.onclick = function () 
-{
-    targetDiv.classList.toggle("notify");
-};
